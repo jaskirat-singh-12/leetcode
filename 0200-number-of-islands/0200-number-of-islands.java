f28bf9fa4251332ab0636ce8b9fe075b13396f00
Jaskirat <69048842+jaskirat-singh-12@users.noreply.github.com>
2026-07-01 03:08:43 +0530

    Time: 4 ms (45.63%), Space: 52.2 MB (67.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,12 +1,4 @@
-class Pair{
-    int first;
-    int sec;
 
-    public Pair(int first, int sec) {
-        this.first = first;
-        this.sec = sec;
-    }
-}
 class Solution {
     public int numIslands(char[][] grid) {
         
@@ -18,39 +10,26 @@ class Solution {
             for(int j = 0; j < grid[0].length; j++){
                 if(!vis[i][j] && grid[i][j] == '1') {
                     ans++;
-                    bfs(grid, i, j, vis);
+                    dfs(grid, i, j, vis);
                 }
             }
         }
         return ans;
     }
-    public void bfs(char[][] graph, int i, int j, boolean[][] vis) {
-        
-        Queue<Pair> q = new LinkedList<>();
-        q.offer(new Pair(i, j));
+    public void dfs(char[][] graph, int i, int j, boolean[][] vis) {
+        if(i < 0 || j < 0 || i >= graph.length || j >= graph[0].length) {
+            return;
+        }
+        if(vis[i][j] || graph[i][j] == '0') {
+            return;
+        }
+
         vis[i][j] = true;
 
-        int[] dr = {-1, 1, 0, 0};
-        int[] dc = {0, 0, -1, 1};
-
-        while(!q.isEmpty()) {
-            int row = q.peek().first;
-            int col = q.peek().sec;
-
-            q.remove();
-
-            for(int k = 0 ; k < 4; k++) {
-                int nr = row + dr[k];
-                int nc = col + dc[k];
-
-                if(nr >= 0 && nc >= 0 &&
-                   nr < graph.length && nc < graph[0].length &&
-                   graph[nr][nc] == '1' && !vis[nr][nc]) {
-
-                    vis[nr][nc] = true;
-                    q.offer(new Pair(nr, nc));
-                }
-            }
-        }
+        dfs(graph, i-1, j, vis);
+        dfs(graph, i, j-1, vis);
+        dfs(graph, i, j+1, vis);
+        dfs(graph, i+1, j, vis);
+        
     }
 }
