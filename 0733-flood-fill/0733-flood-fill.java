8813732ce3cd6877fa4c15c75f289bcdb62bd934
Jaskirat <69048842+jaskirat-singh-12@users.noreply.github.com>
2026-07-02 02:38:58 +0530

    Time: 2 ms (12.01%), Space: 47 MB (11.2%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,21 +1,47 @@
 class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] vis = new boolean[image.length][image[0].length];
+    int[] dr = {-1, 1, 0, 0};
+    int[] dc = {0, 0, -1, 1};
 
-        dfs(image, image[sr][sc], sr, sc, color, vis);
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+        boolean vis[][] = new boolean[image.length][image[0].length];
+
+        bfs(image, image[sr][sc], sr, sc, color, vis);
         return image;
     }
+    public void bfs(int[][] image, int idealColor, int i, int j, int color, boolean[][] vis) {
+        Queue<Pair> q = new LinkedList<>();
 
-    public void dfs(int[][] image, int idealColor, int i, int j, int color, boolean[][] vis) {
-        if(i < 0 || j < 0 || i >= image.length || j >= image[0].length || vis[i][j] || image[i][j] != idealColor) {
-            return;
-        }
         vis[i][j] = true;
+        q.offer(new Pair(i, j));
         image[i][j] = color;
 
-        dfs(image, idealColor, i-1, j, color, vis);
-        dfs(image, idealColor, i+1, j, color, vis);
-        dfs(image, idealColor, i, j-1, color, vis);
-        dfs(image, idealColor, i, j+1, color, vis);
+        while(!q.isEmpty()) {
+            Pair p = q.poll();
+
+            int row = p.i;
+            int col = p.j;
+
+            for(int k = 0; k < 4; k++) {
+                int nr = row+ dr[k];
+                int nc = col + dc[k];
+
+                if(nr >= 0 && nc >= 0 && nr < image.length && nc < image[0].length && image[nr][nc] == idealColor && !vis[nr][nc]) {
+                    vis[nr][nc] = true;
+                    image[nr][nc] = color;
+                    q.offer(new Pair(nr, nc));
+                }
+            }
+        }
+    }
+}
+
+class Pair {
+    int i;
+    int j;
+
+    public Pair(int i, int j) {
+        this.i = i;
+        this.j = j;
     }
 }
