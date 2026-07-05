54bf276cc41c8ebc4953a0d2ff430b1232bc2a6f
Jaskirat <69048842+jaskirat-singh-12@users.noreply.github.com>
2026-07-06 01:20:01 +0530

    Time: 0 ms (100%), Space: 41.8 MB (93.22%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,17 @@
 class Solution {
     public int fib(int n) {
-        return fiboRecursion(n);
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return fiboRecursion(n, dp);
     }
 
-    public int fiboRecursion(int n) {
-        if(n == 0 || n == 1) return n;
+    public int fiboRecursion(int n, int[] dp) {
+        if(n == 0 || n == 1) return dp[n] = n;
 
-        return fiboRecursion(n-1) + fiboRecursion(n-2);
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+
+        return dp[n] = fiboRecursion(n-1, dp) + fiboRecursion(n-2, dp);
     }
 }
