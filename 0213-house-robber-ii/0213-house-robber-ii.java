3eb5e0eb5350d27331a5c2899c1d1eb99c54fd11
Jaskirat <69048842+jaskirat-singh-12@users.noreply.github.com>
2026-08-11 01:38:25 +0530

    Time: 0 ms (100%), Space: 42.6 MB (90.16%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,33 +1,22 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 1) {
-            return nums[0];
-        }
-
-        if(n == 2) {
-            return Math.max(nums[0], nums[1]);
-        }
-
+        if(n == 1) return nums[0];
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
-
-        int take1 = rob(nums, 0, n-2, dp);
+        int take1 = rob(nums, 0, n-1, dp);
         Arrays.fill(dp, -1);
-        int take2 = rob(nums, 1, n-1, dp);
+        int take2 = rob(nums, 1, n, dp);
         return Math.max(take1, take2);
     }
 
     public int rob(int[] nums, int i, int n, int[] dp) {
-        if(i > n) {
-            return 0;
-        }
+        if(i >= n) return 0; //n-1
         if(dp[i] != -1) return dp[i];
 
-        int take = nums[i] + rob(nums, i+2, n, dp);
+        int take = nums[i] + rob(nums, i+2, n, dp); 
         int skip = rob(nums, i+1, n, dp);
 
         return dp[i] = Math.max(take, skip);
     }
 }
-
