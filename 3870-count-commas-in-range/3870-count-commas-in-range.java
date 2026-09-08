02f7181ddb34c13926b25eef73faf5760ba680c6
Jaskirat <69048842+jaskirat-singh-12@users.noreply.github.com>
2026-09-08 17:00:42 +0530

    Time: 1 ms (83.65%), Space: 42.5 MB (72.85%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countCommas(int n) {
+        if(n <= 999) {
+            return 0;
+        }
+        if(n >= 1000 && n <= 100000) {
+            return n - 999;
+        }
+        return n;
+    }
+}
