edb0c2332f171571ce7d731bab67def0d5361bc5
Jaskirat <69048842+jaskirat-singh-12@users.noreply.github.com>
2026-09-10 18:54:13 +0530

    Time: 1 ms (56.32%), Space: 46.3 MB (9.59%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.java
@@ -22,35 +22,40 @@ class Solution {
     }
     int i = 1;
 
-    public void findAvg(TreeNode root) {
+    public int[] findAvg(TreeNode root) {
         if(root == null) {
-            return;
+            return new int[] {0,0};
         }
-        i = 1;
-        long sum = findSum(root);
+
+        int[] left = new int[2];
+        int[] right = new int[2];
+
+        if(root.left != null) left = findAvg(root.left);
+        if(root.right != null) right = findAvg(root.right);
+
+        long sum = left[0] + right[0] + root.val;
+        int i = left[1] + right[1] + 1;
 
         if(sum / i == root.val) {
             ans++;
         }
-        if(root.left != null) findAvg(root.left);
-        if(root.right != null) findAvg(root.right);
 
-        return;
+        return new int[] {(int) sum, i};
     }
 
-    private long findSum(TreeNode root) {
-        if(root.left == null && root.right == null) return root.val;
-        long left = 0,right = 0;
+    // private long findSum(TreeNode root) {
+    //     if(root.left == null && root.right == null) return root.val;
+    //     long left = 0,right = 0;
 
-        if(root.left != null){
-            i++;
-            left = findSum(root.left);
-        }
-        if(root.right != null){
-            i++;
-            right = findSum(root.right);
-        }
+    //     if(root.left != null){
+    //         i++;
+    //         left = findSum(root.left);
+    //     }
+    //     if(root.right != null){
+    //         i++;
+    //         right = findSum(root.right);
+    //     }
 
-        return left + right + root.val ;
-    }
+    //     return left + right + root.val ;
+    // }
 }
